3c17462d5939b29c5ff05a5ad27c0a901e86bb0a
Orjan Ameye <orjan.ameye@hotmail.com>
2025-04-26 07:51:02 +0200

    build: allow BijectiveHilbert 0.5 (#36)

--- test/sorting.jl
+++ test/sorting.jl
@@ -1,5 +1,5 @@
 
-using HarmonicSteadyState
+using HarmonicSteadyState, HarmonicBalance, Test
 
 @variables ω₀ γ λ α ω t x(t)
 
@@ -14,10 +14,13 @@ fixed = (ω₀ => 1.0, γ => 0.002, α => 1.0)
 varied = (ω => range(0.99, 1.01, 10), λ => range(1e-6, 0.03, 10))
 
 @testset "hilbert" begin
-    result_2D = get_steady_states(harmonic_eq, varied, fixed; verbose=true)
-    result_2D_hilbert = get_steady_states(
-        harmonic_eq, varied, fixed; sorting="hilbert", verbose=true
-    )
+    result_2D = get_steady_states(harmonic_eq, varied, fixed)
+    result_2D_hilbert = get_steady_states(harmonic_eq, varied, fixed; sorting="hilbert")
+    is_zero_solution(v) = all(round.(v) .≈ 0.0 + 0.0*im)
+    maps = map(zip(result_2D.solutions, result_2D_hilbert.solutions)) do (sol, sol_hilbert)
+        is_zero_solution.(sol) == is_zero_solution.(sol_hilbert)
+    end
+    @test all(maps)
 end
 
 # ∨ compare sorting solutions
