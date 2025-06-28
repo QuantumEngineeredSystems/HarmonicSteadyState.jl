6198d4968742cba2a6d281d4b88f94f344851bd7
Orjan Ameye <orjan.ameye@hotmail.com>
2025-06-28 21:48:07 +0200

    try more things

--- attractors.jl
+++ attractors.jl
@@ -1,6 +1,8 @@
-using HarmonicBalance, ModelingToolkit
+using HarmonicBalance, HarmonicSteadyState
+using Symbolics, RuntimeGeneratedFunctions, QuestBase, FunctionWrappers, SciMLBase
+using ModelingToolkit
+import FunctionWrappers: FunctionWrapper
 using Attractors
-
 
 @variables γ λ α ω0 ω t
 @variables x(t)
@@ -10,16 +12,29 @@ dEOM = DifferentialEquation(nat_eq, x)
 add_harmonic!(dEOM, x, ω)
 harmonic_eq = get_harmonic_equations(dEOM);
 
-using OrdinaryDiffEq
-param_1d = Dict(
-    Δ => Delta / omega0, γ => gamma / omega0, F₁ => F1_fix, δ => δ_exp / omega0, F₂ => 1e-4)
-prob = ODEProblem(harmonic_eq, SA[rand(6)...] ./ 4, (0.0, 100.0), param_1d; in_place=false)
-diffeq = (alg=Vern7(),abstol = 1e-6,reltol = 1e-6)
-ds = CoupledODEs(prob, #=diffeq=#)
+struct AttractorsProblem{
+    ParType<:Number,
+    Jac<:JacobianFunction(ComplexF64),
+} <: SteadyStateProblem
+    variables::Vector{Num}
+    parameters::Vector{Num}
+    swept_parameters::OrderedDict{Num,Vector{ParType}}
+    fixed_parameters::OrderedDict{Num,ParType}
+    phase_space::NTuple{N,Vector{ParType}}
+    coupled_ode::DynamicalSystemBase.CoupledODEs
+    jacobian::Jac
+    eom::HarmonicEquation
+end
 
-set_parameter!(ds, :F₂, 0.0007)
+ODEProblem(
+    harmonic_eq,
+    zeros(2),
+    (0, Inf),
+    Dict(ω0 => 1.0, γ => 0.1, λ => 0.05, α => 0.01),
+)
 
 u_range = range(-0.2, 0.2; length=75)
+u_range |> typeof
 grid = (
     u_range, # u1
     u_range, # v1
@@ -50,3 +65,29 @@ reinit!(ds)
 fractions_cont, attractors_cont = global_continuation(
     algo, continuation_range, :F₂, sampler; samples_per_parameter=50
 )
+
+
+# eqs = rearrange_standard(harmonic_eq).equations
+# rhs = Num[eq.lhs for eq in eqs]
+# vars = get_variables(harmonic_eq)
+
+# function compile_function(
+#     rhs::Vector{Num}, variables::Vector{Num}; rules=Dict()
+# )::RuntimeGeneratedFunction
+#     rhss = QuestBase.substitute_all.(rhs, Ref(rules)) # Ref makes sure only mat is broadcasted
+#     rhsf = Symbolics.build_function(rhss, variables; expression=Val(false))
+#     return rhsf isa Tuple ? first(rhsf) : rhsf
+# end
+
+# rgf = compile_function(
+#     rhs, vcat(vars, ω); rules=Dict(ω0 => 1.0, γ => 0.1, λ => 0.05, α => 0.01)
+# ) # RuntimeGeneratedFunction
+
+# function f(u,p,t)
+#     rgf(vcat(u,p))
+# end
+# ff = FunctionWrapper{Vector{Float64},Tuple{Vector{Float64},Vector{Float64},Float64}}(f)
+# ff(rand(2), rand(1), 0.0)
+
+# CoupledODEs(ff, rand(2) , rand(1))
+# SciMLBase.isinplace(ff, 100)
